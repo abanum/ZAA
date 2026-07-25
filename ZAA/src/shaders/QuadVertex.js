99040be8a40eece784881e8cb3cdfd00ecdc2f91
abanum <mabu@ff.iij4u.or.jp>
2026-07-25 10:39:43 +0900

    Move project to repo root for GitHub Pages

--- ZAA/src/shaders/QuadVertex.js
+++
@@ -1,9 +0,0 @@
-export const QUAD_VERTEX_SOURCE = `#version 300 es
-precision highp float;
-in vec2 aPos;
-out vec2 vUv;
-void main() {
-  vUv = aPos * 0.5 + 0.5;
-  gl_Position = vec4(aPos, 0.0, 1.0);
-}
-`;
